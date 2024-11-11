a99525ae854b73413f25a170745f84f4a8cdd025
dearpollapat <pollapat.dear@gmail.com>
2024-11-11 20:15:37 +0700

    create route

---
+++ index.js
@@ -0,0 +1,11 @@
+
+import  getPosts, {getPostLength}  from "./postController.js";
+console.log(getPosts());
+console.log(getPostLength());
+
+
+// const {generateRandomNumber , celciusToFahrenheit} = require('./utils')
+
+// console.log(`Random number : ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenhiet : ${celciusToFahrenheit(0)}`);
